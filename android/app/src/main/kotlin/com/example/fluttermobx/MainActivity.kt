14d2e6e6b6b072a971c3fc5881510b34dbf45969
Maíssa Maniezzo <maissamaniezzo@gmail.com>
2021-08-22 15:56:15 -0300

    Aulas #1 e #2

---
+++ android/app/src/main/kotlin/com/example/fluttermobx/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fluttermobx
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:fluttermobx/controller.dart';
+import 'controller.dart';
+
+class MyHomePage extends StatelessWidget {
+  final controller = Controller();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MobX'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Observer(builder: (_) {
+              return Text(
+                '${controller.counter}',
+                style: Theme.of(context).textTheme.headline4,
+              );
+            })
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {controller.increment();},
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
